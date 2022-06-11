d061cf1733b35be615d29fc34dd8f49282c3a3c9
Avinash Kethireddy <avinashkethireddy.reddy@gmail.com>
2022-06-11 13:36:17 +0530

    updated portfolio

--- js/main.js
+++ js/main.js
@@ -1,52 +1,242 @@
-// Display of loading screen and adjustment of body elements with respect to navigation bar, while loading the page in browser.
-$(window).on("load", function () {
-  $("body").css("padding-top", $("#navmenu").height());
-  $("#outerNavbar ul li a").click(function (e) {
-    e.preventDefault();
-    let navigationHeight = $("#navmenu").outerHeight();
-    let myHref = $(this).attr("href");
-    let newPos = $(myHref).offset().top;
-    window.scroll(0, newPos - navigationHeight);
-  });
-});
+// // Display of loading screen and adjustment of body elements with respect to navigation bar, while loading the page in browser.
+// $(window).on("load", function () {
+//   $("body").css("padding-top", $("#navmenu").height());
+//   $("#outerNavbar ul li a").click(function (e) {
+//     e.preventDefault();
+//     let navigationHeight = $("#navmenu").outerHeight();
+//     let myHref = $(this).attr("href");
+//     let newPos = $(myHref).offset().top;
+//     window.scroll(0, newPos - navigationHeight);
+//   });
+// });
 
-// Selection of active navlink in navigation bar, while scrolling through the webpage.
-$(window)
-  .scroll(function () {
-    let navigationHeight = $("#navmenu").outerHeight();
-    let scrollDistance = $(window).scrollTop();
-    $(".navsection").each(function (i) {
-      console.log($(this).position().top);
-      if ($(this).position().top <= scrollDistance + navigationHeight) {
-        $(".navbar-nav").find("li.active").removeClass("active");
-        $(".navbar-nav li").eq(i).addClass("active");
+// // Selection of active navlink in navigation bar, while scrolling through the webpage.
+// $(window)
+//   .scroll(function () {
+//     let navigationHeight = $("#navmenu").outerHeight();
+//     let scrollDistance = $(window).scrollTop();
+//     $(".navsection").each(function (i) {
+//       console.log($(this).position().top);
+//       if ($(this).position().top <= scrollDistance + navigationHeight) {
+//         $(".navbar-nav").find("li.active").removeClass("active");
+//         $(".navbar-nav li").eq(i).addClass("active");
+//       }
+//     });
+//   })
+//   .scroll();
+
+// // Adjustment of body elements with respect to navigation bar, while resizing browser window.
+// $(window).resize(function () {
+//   $("body").css("padding-top", $("#navbar").height() - 53);
+// });
+
+// // Opening (and activating) of the relevant Skillset tab and closing all other Skillset tabs.
+// function openSkill(evt, skillName) {
+//   let i, tabcontent, tablinks;
+
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   document.getElementById(skillName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+
+// openSkill(event, 'Prog');
+
+// ======================================================>
+
+!(function($) {
+  "use strict";
+
+  // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
       }
     });
-  })
-  .scroll();
+  });
 
-// Adjustment of body elements with respect to navigation bar, while resizing browser window.
-$(window).resize(function () {
-  $("body").css("padding-top", $("#navbar").height() - 53);
-});
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
 
-// Opening (and activating) of the relevant Skillset tab and closing all other Skillset tabs.
-function openSkill(evt, skillName) {
-  let i, tabcontent, tablinks;
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
   }
+  $(window).on('load', function() {
+    aos_init();
+  });
 
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(skillName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
-openSkill(event, 'Prog');
+})(jQuery);
